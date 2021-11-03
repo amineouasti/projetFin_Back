c75b928af25e1fe57f220d4252eeb70cab0f19a6
amine <aminemomo123@gmail.com>
2021-11-03 12:55:34 +0100

    first push for login

--- api/classes.py
+++ api/classes.py
@@ -1,6 +1,20 @@
+from . import app ,db
+from flask import jsonify, request, session
+from core.authentification import token_required
+from model.classification import classification
+@app.route('/classesList/<idUser>', methods=['GET'])
+@token_required
+def classes(idUser):
+    
+    classesList = [{"id": classe.id ,"name":classe.name} for classe in classification.query.filter_by(id_user =idUser).all()]
 
-from . import app 
+    return jsonify( classesList)
+@app.route('/classe', methods=['POST'])
+@token_required
+def classeInsert():
+    data = request.json
+    newClasse = classification(name = data['name'], id_user = data['id_user'])
+    db.session.add(newClasse)
+    db.session.commit()
 
-@app.route('/classes', methods=['GET'])
-def Classes():
-    return 'it works'
+    return {"ok":"ok"}
--- api/login.py
+++ api/login.py
@@ -1,15 +1,23 @@
 
 from . import app ,db
-from flask import jsonify,request, make_response
+from flask import jsonify,request, make_response, session
 import jwt
 from model.users import users
+import datetime
+
+app.permanent_session_lifetime = datetime.timedelta(minutes=30)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
     auth = request.authorization
-    if auth and auth.password == 'test' :
-        token = jwt.encode({'email' : auth.username}, app.config['SECRET_KEY'],algorithm="HS256" )
-        return jsonify({'token':token})
-    usersList = [{"email":user.email} for user in users.query.all()]
+    
+    if auth :
+        user = users.query.filter_by(email = auth.username,password =auth.password ).first()
+        if user:
+            session.permanent = True
+            token = jwt.encode({'email' : auth.username,'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'],algorithm="HS256" )
+        
+            return jsonify({'token':token})
+    
 
     return make_response('could not verify!', 401, {'Auth':'Basic ralm="login required"'})
